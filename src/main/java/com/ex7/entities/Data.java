368db7eda32a908f5f495cd4d2f035c57fa6f6ee
Nikolay Grunin <groonyn@gmail.com>
2018-02-23 01:02:28 +0300

    ex8 - corrections after Dmitry's comments

--- src/main/java/com/ex7/entities/Data.java
+++ src/main/java/com/ex7/entities/Data.java
@@ -2,15 +2,17 @@ package com.ex7.entities;
 
 import com.epam.commons.DataClass;
 import com.ex7.enums.*;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class Data extends DataClass {
-    private List<NumbersEnum> summary;
-    private List<ElementsEnum> elements;
-    private ColoursEnum colour;
-    private MetalsEnum metals;
-    private List<SaladEnum> salad;
+    private final List<NumbersEnum> summary;
+    private final List<ElementsEnum> elements;
+    private final ColoursEnum colour;
+    private final MetalsEnum metals;
+    private final List<SaladEnum> salad;
 
     public Data(List<NumbersEnum> summary, List<ElementsEnum> elements, ColoursEnum colour, MetalsEnum metals, List<SaladEnum> salad) {
         this.summary = summary;
@@ -18,25 +20,5 @@ public class Data extends DataClass {
         this.colour = colour;
         this.metals = metals;
         this.salad = salad;
-    }
-
-    public List<NumbersEnum> getSummary() {
-        return summary;
-    }
-
-    public List<ElementsEnum> getElements() {
-        return elements;
-    }
-
-    public ColoursEnum getColour() {
-        return colour;
-    }
-
-    public MetalsEnum getMetals() {
-        return metals;
-    }
-
-    public List<SaladEnum> getSalad() {
-        return salad;
     }
 }
