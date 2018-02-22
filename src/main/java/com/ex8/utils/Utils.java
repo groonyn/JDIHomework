ff4990450e39ecab88e7a4fc67ccd2955bed0d93
Nikolay Grunin <groonyn@gmail.com>
2018-02-23 00:24:50 +0300

    ex8 - corrections after Dmitry's comments

--- src/main/java/com/ex8/utils/Utils.java
+++ src/main/java/com/ex8/utils/Utils.java
@@ -1,12 +1,12 @@
 package com.ex8.utils;
 
 import com.ex8.entities.Data;
-import com.google.common.io.Files;
 import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
@@ -15,7 +15,8 @@ import java.util.List;
 
 public class Utils {
     public static List<Data> jsonGetData() throws IOException {
-        String s = Files.toString(new File("src\\main\\resources\\ex8_jdi_metalsColorsDataSet.json"), Charset.defaultCharset());
+        FileInputStream fileInputStream = new FileInputStream("src\\main\\resources\\ex8_jdi_metalsColorsDataSet.json");
+        String s = IOUtils.toString(fileInputStream, Charset.defaultCharset());
         Gson gson = new Gson();
         Type type = new TypeToken<LinkedTreeMap<String, Data>>() {
         }.getType();
