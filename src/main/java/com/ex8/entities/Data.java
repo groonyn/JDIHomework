46622c2754f94c9c7f9f80e92a9e6bf9e91465a5
Nikolay Grunin <groonyn@gmail.com>
2018-02-23 00:08:01 +0300

    ex8 - corrections after Dmitry's comments

--- src/main/java/com/ex8/entities/Data.java
+++ src/main/java/com/ex8/entities/Data.java
@@ -1,31 +1,14 @@
 package com.ex8.entities;
 
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
 public class Data {
     private List<Integer> summary;
     private List<String> elements;
     private String color;
     private String metals;
     private List<String> vegetables;
-
-    public List<Integer> getSummary() {
-        return summary;
-    }
-
-    public List<String> getElements() {
-        return elements;
-    }
-
-    public String getColour() {
-        return color;
-    }
-
-    public String getMetals() {
-        return metals;
-    }
-
-    public List<String> getSalad() {
-        return vegetables;
-    }
 }
--- src/main/java/com/ex8/entities/User.java
+++ src/main/java/com/ex8/entities/User.java
@@ -1,13 +1,12 @@
 package com.ex8.entities;
 
-import com.epam.commons.DataClass;
 import com.ex8.enums.AccountEnum;
 
-public class User extends DataClass {
+public class User {
 
-    private String login;
-    private String password;
-    private String user;
+    private final String login;
+    private final String password;
+    private final String user;
 
     public User(AccountEnum accountEnum) {
         this.login = accountEnum.login;
