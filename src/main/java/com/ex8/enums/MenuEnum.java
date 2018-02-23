4d3aa3b89968aa7f0b01621e8ceb559c765b92dc
Nikolay Grunin <groonyn@gmail.com>
2018-02-23 16:22:56 +0300

    ex8

---
+++ src/main/java/com/ex8/enums/MenuEnum.java
@@ -0,0 +1,11 @@
+package com.ex8.enums;
+
+public enum MenuEnum {
+    METALSCOLORS("METALS & COLORS");
+
+    public String menu;
+
+    MenuEnum(String menu) {
+        this.menu = menu;
+    }
+}
