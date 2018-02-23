234ccd0df4d5b5f123c3c5595c2e1371a8a24221
Nikolay Grunin <groonyn@gmail.com>
2018-02-23 16:26:48 +0300

    ex8

--- src/test/java/com/ex8/LoginTestDataProvider.java
+++ src/test/java/com/ex8/LoginTestDataProvider.java
@@ -4,13 +4,14 @@ import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import com.ex8.entities.Data;
 import com.ex8.entities.User;
-import com.ex8.enums.AccountEnum;
 import com.ex8.site.Site;
 import com.ex8.utils.Utils;
 import org.testng.annotations.*;
 
 import java.io.IOException;
 
+import static com.ex8.enums.AccountEnum.PITER_CHAILOVSKI;
+import static com.ex8.enums.MenuEnum.METALSCOLORS;
 import static com.ex8.site.Site.mainPage;
 import static com.ex8.site.Site.metalColoursPage;
 import static com.ex8.site.pages.MetalColoursPage.provideExpectedResult;
@@ -26,7 +27,8 @@ public class LoginTestDataProvider extends TestNGBase {
     @BeforeMethod
     public void setUpMethod() {
         mainPage.open();
-        mainPage.login(new User(AccountEnum.PITER_CHAILOVSKI)).clickMetalColourLink();
+        mainPage.login(new User(PITER_CHAILOVSKI));
+        mainPage.headerMenu.selectMenu(METALSCOLORS);
         metalColoursPage.checkOpened();
     }
 
